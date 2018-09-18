b99a9984510861c0544f6f41616dd579be354a25
alam5827 <42847203+alam5827@users.noreply.github.com>
2018-09-19 03:12:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+for (var i = 0; i < names.length; i++) {
+
+  
+  var firstLetter = names[i].charAt().toLowerCase();
+
+  
+  if (firstLetter === 'j') {
+    console.log("Good Bye "+names[i]);
+  } 
+  else {
+    console.log("Hello "+names[i]);
+  }
+}
+
+})();
